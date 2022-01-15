8ab4686f683138e12f48435cc67d0d6f552c0ed3
Ty Butler <tytbu@tytbutler.com>
2022-01-15 09:18:53 -0600

    Cleaned up ExtendedTrajectoryUtils No more vars plase stop using var its bad

--- src/main/java/frc/robot/utility/ExtendedTrajectoryUtilities.java
+++ src/main/java/frc/robot/utility/ExtendedTrajectoryUtilities.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.controller.HolonomicDriveController;
@@ -19,11 +20,13 @@ import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.trajectory.*;
 import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstraint;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystem.swerve.pathfollowingswerve.PathFollowingSwerve;
 import frc.robot.subsystem.swerve.pathfollowingswerve.command.FollowDottedTrajectoryCommand;
 import frc.robot.subsystem.swerve.pathfollowingswerve.command.FollowStatesCommand;
@@ -32,10 +35,11 @@ import frc.robot.subsystem.swerve.pathfollowingswerve.command.FollowTrajectoryCo
 
 
 /**
- * Add your docs here.
+ * Utilities for running autonomous paths
+ * A bunch of the included methods will not be used, for the most part we will only be using getDeployedTrajectory()
  */
 public class ExtendedTrajectoryUtilities {
-    public static Trajectory getDeployedTrajectory(String trajectoryName) throws IOException {
+    private static Trajectory getDeployedTrajectoryExcept(String trajectoryName) throws IOException {
 
         var trajectoryJSON = "paths/output/"+trajectoryName+".wpilib.json";
         //Stolen pretty much from the example code
@@ -43,9 +47,9 @@ public class ExtendedTrajectoryUtilities {
         Trajectory trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
         return trajectory;
     }
-    public static Trajectory tryGetDeployedTrajectory(String trajectoryName){
+    public static Trajectory getDeployedTrajectory(String trajectoryName){
         try{
-            return getDeployedTrajectory(trajectoryName);
+            return getDeployedTrajectoryExcept(trajectoryName);
         }catch(IOException ex){
             DriverStation.reportError("Unable to open trajectory: " + trajectoryName, ex.getStackTrace());
             return new Trajectory(null);
@@ -54,18 +58,24 @@ public class ExtendedTrajectoryUtilities {
     public static Trajectory regenerateTrajectory(Trajectory trajectory, TrajectoryConfig config){
         return TrajectoryGenerator.generateTrajectory(Arrays.asList(trajectory.getStates().stream().map(s -> s.poseMeters).toArray(Pose2d[]::new)), config);
     }
-    public static CommandBase addTrajectoryWithShuffleboard(PathFollowingSwerve swerve, String tabName, String trajectoryName){
 
-        var tab = Shuffleboard.getTab(tabName);
-        var timeAdvanceEntry = tab.addPersistent("Time Advance", 0.02).getEntry();
-        var distanceMultiplerEntry = tab.addPersistent("Distance Multiplier", 3).getEntry();
+    /**
+     * Creates a trajectory command and adds it to Shuffleboard
+     * @param swerve The swerve that runs the path
+     * @param tabName the tab on which to place the command and info
+     * @param trajectoryName the name of the trajectory to run
+     * @return An InstantCommand wrapper for the FollowStatesCommand
+     */
+    public static CommandBase addTrajectoryWithShuffleboard(PathFollowingSwerve swerve, String tabName, String trajectoryName){ //Adds a trajectory with a shuffleboard tab for controlling said trajectory
 
-        var trajectory = tryGetDeployedTrajectory(trajectoryName);
-        var followCommand = new FollowStatesCommand(swerve, trajectory, createBasicController(1, 1, 1, 4, 1), distance -> {
+        ShuffleboardTab tab = Shuffleboard.getTab(tabName);
+        NetworkTableEntry timeAdvanceEntry = tab.addPersistent("Time Advance", 0.02).getEntry();
+        NetworkTableEntry distanceMultiplerEntry = tab.addPersistent("Distance Multiplier", 3).getEntry();
+
+        Trajectory trajectory = getDeployedTrajectory(trajectoryName);
+        FollowStatesCommand followCommand = new FollowStatesCommand(swerve, trajectory, createBasicController(1, 1, 1, 4, 1), distance -> {
             return Math.min(timeAdvanceEntry.getDouble(0.02), 1/(distance * distanceMultiplerEntry.getDouble(3)));
         });
-
-
 
         createTrajectoryRegenerationLayout(swerve, tab, trajectory, followCommand);
 
@@ -75,10 +85,18 @@ public class ExtendedTrajectoryUtilities {
 
     }
 
+    /**
+     * Creates a dotted trajectory command and adds it to shuffleboard
+     * @param swerve The PathFollowingSwerve to run the command
+     * @param tabName The tab on which to add the command
+     * @param trajectoryName The name of the trajectory
+     * @return An InstantCommand wrapper for the DottedTrajectoryCommand
+     */
+
     public static CommandBase addDottedTrajectoryWithShuffleboard(PathFollowingSwerve swerve, String tabName, String trajectoryName){
-        var tab = Shuffleboard.getTab(tabName);
-        var trajectory = tryGetDeployedTrajectory(trajectoryName);
-        var followCommand = new FollowDottedTrajectoryCommand(swerve, trajectory, createBasicController(1, 1, 1, 4, 1), 0.01, 0.02);
+        ShuffleboardTab tab = Shuffleboard.getTab(tabName);
+        Trajectory trajectory = getDeployedTrajectory(trajectoryName);
+        FollowDottedTrajectoryCommand followCommand = new FollowDottedTrajectoryCommand(swerve, trajectory, createBasicController(1, 1, 1, 4, 1), 0.01, 0.02);
         createTrajectoryRegenerationLayout(swerve, tab, trajectory, followCommand);
 
         createControllerRegenerationLayout(swerve, tab, followCommand);
@@ -88,9 +106,9 @@ public class ExtendedTrajectoryUtilities {
 
 
     public static CommandBase addDottedTrajectoryWithShuffleboard(PathFollowingSwerve swerve, String tabName, String trajectoryName, boolean rotation, double rotationOffset){
-        var tab = Shuffleboard.getTab(tabName);
-        var trajectory = tryGetDeployedTrajectory(trajectoryName);
-        var followCommand = new FollowDottedTrajectoryCommand(swerve, trajectory, createBasicController(1, 1, 1, 4, 1), 0.01, 0.02, rotation, rotationOffset);
+        ShuffleboardTab tab = Shuffleboard.getTab(tabName);
+        Trajectory trajectory = getDeployedTrajectory(trajectoryName);
+        FollowDottedTrajectoryCommand followCommand = new FollowDottedTrajectoryCommand(swerve, trajectory, createBasicController(1, 1, 1, 4, 1), 0.01, 0.02, rotation, rotationOffset);
         createTrajectoryRegenerationLayout(swerve, tab, trajectory, followCommand);
 
         createControllerRegenerationLayout(swerve, tab, followCommand);
@@ -98,18 +116,18 @@ public class ExtendedTrajectoryUtilities {
         return createAndAddFinalFollowCommand(swerve, tab, trajectory, followCommand);
     }
     private static CommandBase createAndAddFinalFollowCommand(PathFollowingSwerve swerve, ShuffleboardTab tab, Trajectory trajectory, FollowTrajectoryCommand  followCommand) {
-        var realFollow = new InstantCommand(() -> swerve.resetPose(trajectory.getInitialPose().getTranslation()), swerve).andThen(followCommand);
-        tab.add("Follow Command Diagnostics", followCommand);
-        tab.add("Run Follow Command", realFollow);
+        SequentialCommandGroup realFollow = new InstantCommand(() -> swerve.resetPose(trajectory.getInitialPose().getTranslation()), swerve).andThen(followCommand);
+        tab.add("Follow Command Diagnostics", followCommand); //path command
+        tab.add("Run Follow Command", realFollow); //Command which runs the path command
         return realFollow;
     }
     public static void createControllerRegenerationLayout(PathFollowingSwerve swerve, ShuffleboardTab tab, FollowTrajectoryCommand followCommand) {
-        var controllerLayout = tab.getLayout("Controller Regeneration", BuiltInLayouts.kList);
-        var kPxEntry = controllerLayout.add("kPx", 1).getEntry();
-        var kPyEntry = controllerLayout.add("kPy", 1).getEntry();
-        var kPwEntry = controllerLayout.add("kPw", 1).getEntry();
-        var maxRotSpeedEntry = controllerLayout.add("Max Rotational Speed", 4).getEntry();
-        var maxRotAccelerationEntry = controllerLayout.add("Max Rotational Acceleration", 1).getEntry();
+        ShuffleboardLayout controllerLayout = tab.getLayout("Controller Regeneration", BuiltInLayouts.kList);
+        NetworkTableEntry kPxEntry = controllerLayout.add("kPx", 1).getEntry();
+        NetworkTableEntry kPyEntry = controllerLayout.add("kPy", 1).getEntry();
+        NetworkTableEntry kPwEntry = controllerLayout.add("kPw", 1).getEntry();
+        NetworkTableEntry maxRotSpeedEntry = controllerLayout.add("Max Rotational Speed", 4).getEntry();
+        NetworkTableEntry maxRotAccelerationEntry = controllerLayout.add("Max Rotational Acceleration", 1).getEntry();
         controllerLayout.add("Regenerate Controller", new InstantCommand(() -> {
             followCommand.setController(createBasicController(
                     kPxEntry.getDouble(1),
@@ -120,13 +138,13 @@ public class ExtendedTrajectoryUtilities {
         }, swerve));
     }
     public static void createTrajectoryRegenerationLayout(PathFollowingSwerve swerve, ShuffleboardTab tab, Trajectory trajectory, FollowTrajectoryCommand followCommand) {
-        var trajectoryLayout = tab.getLayout("Trajectory Regeneration", BuiltInLayouts.kList);
-        var maxVelocityEntry = trajectoryLayout.add("Max Velocity Meters", 2.4).getEntry();
-        var maxAccelerationEntry = trajectoryLayout.add("Max Acceleration Meters", 0.5).getEntry();
-        var maxCentripetalAccelerationEntry = trajectoryLayout.add("Max Centripetal Acceleration Meters", 0.5).getEntry();
+        ShuffleboardLayout trajectoryLayout = tab.getLayout("Trajectory Regeneration", BuiltInLayouts.kList);
+        NetworkTableEntry maxVelocityEntry = trajectoryLayout.add("Max Velocity Meters", 2.4).getEntry();
+        NetworkTableEntry maxAccelerationEntry = trajectoryLayout.add("Max Acceleration Meters", 0.5).getEntry();
+        NetworkTableEntry maxCentripetalAccelerationEntry = trajectoryLayout.add("Max Centripetal Acceleration Meters", 0.5).getEntry();
         trajectoryLayout.add("Regenerate Trajectory", new InstantCommand(() -> {
 
-            var config =  new TrajectoryConfig(
+            TrajectoryConfig config =  new TrajectoryConfig(
                     maxVelocityEntry.getDouble(2.4),
                     maxAccelerationEntry.getDouble(0.5));
             config.addConstraint(new CentripetalAccelerationConstraint(maxCentripetalAccelerationEntry.getDouble(0.5)));
