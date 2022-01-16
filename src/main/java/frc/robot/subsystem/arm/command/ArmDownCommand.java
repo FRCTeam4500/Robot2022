f6984810eac013b2692a43badc17e4f32cb6bdd9
Ty Butler <tytbu@tytbutler.com>
2022-01-15 21:00:51 -0600

    Better control commands

---
+++ src/main/java/frc/robot/subsystem/arm/command/ArmDownCommand.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystem.arm.command;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ArmDownCommand extends CommandBase {
+}
---
+++ src/main/java/frc/robot/subsystem/loader/command/LoaderRunCommand.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystem.loader.command;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystem.loader.Loader;
+import frc.robot.subsystem.loader.LoaderConstants;
+
+/**
+ * Runs a loader until the command ends
+ * We don't need to provide commands for setting the speed because the loader will never be left on, unlike the arm
+ */
+
+public class LoaderRunCommand extends CommandBase {
+    private Loader loader;
+
+    public LoaderRunCommand(Loader loader){
+        this.loader = loader;
+        addRequirements(loader);
+    }
+
+    public void initialize(){
+        loader.setSpeed(LoaderConstants.runSpeed);
+    }
+}
