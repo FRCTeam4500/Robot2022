0190e07596899484690a3b0e7968d73f9219ce15
Lolzkarry <46729989+Lolzkarry@users.noreply.github.com>
2022-03-03 15:59:55 -0600

    commit climber

--- src/main/java/frc/robot/subsystem/climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystem/climber/ClimberConstants.java
@@ -3,4 +3,5 @@ package frc.robot.subsystem.climber;
 public class ClimberConstants {
     public static final double CLIMBER_UP_ANGLE = 1.1; //TODO: THESE ARE NOT RIGHT
     public static final double CLIMBER_DOWN_ANGLE = -1.1;
+    public static final double MAX_OUTPUT = 0.0; //TODO: SET ALL OF THESE SMH
 }
--- src/main/java/frc/robot/subsystem/climber/ClimberImpl.java
+++ src/main/java/frc/robot/subsystem/climber/ClimberImpl.java
@@ -6,10 +6,14 @@ import frc.robot.subsystem.arm.ArmConstants;
 
 public class ClimberImpl implements Climber {
     private SmartMotorComponent smartMotor;
+    private SmartMotorComponent smartMotor2;
     private double targetAngle = 0; //TODO: Find what angle this is
     private double targetOutput = 0; //TODO: LOL
 
-    public ClimberImpl(SmartMotorComponent smartMotor) { this.smartMotor = smartMotor;}
+    public ClimberImpl(SmartMotorComponent smartMotor, SmartMotorComponent smartMotor2) {
+        this.smartMotor = smartMotor;
+        this.smartMotor2 = smartMotor2;
+    }
 
     @Override
     public void setAngle(double angle) {
@@ -20,16 +24,16 @@ public class ClimberImpl implements Climber {
     @Override
     public void setOutput(double output) {
         targetOutput = output;
-        smartMotor.setOutput(output);
+        smartMotor2.setOutput(output);
     }
 
     @Override
     public void initSendable(SendableBuilder builder){
-        builder.addBooleanProperty("Active", () -> !(targetAngle >= ClimberConstants.CLIMBER_UP_ANGLE), null);
+        builder.addBooleanProperty("Active", () -> !(targetAngle >= 0), null); //TODO: idk how to make it "active" if its either below or above I think this only selects one or the other
         builder.addDoubleProperty("Target angle", () -> targetAngle, null);
         builder.addDoubleProperty("Target output", () -> targetOutput, null);
         builder.addDoubleProperty("currentAngle", () -> smartMotor.getAngle(), null);
-        builder.addDoubleProperty("currentOutput", () -> smartMotor.getOutput(), null);
+        builder.addDoubleProperty("currentOutput", () -> smartMotor2.getOutput(), null);
     }
 
 }
