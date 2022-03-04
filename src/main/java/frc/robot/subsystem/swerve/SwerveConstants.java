426b26edb2a06ec035cc9c0088c0935949bd60af
Ty Butler <tbutler16@claytonschools.net>
2022-03-04 15:52:53 -0600

    Swerve cosntants

---
+++ src/main/java/frc/robot/subsystem/swerve/SwerveConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystem.swerve;
+
+public class SwerveConstants {
+    public static double MAX_LINEAR_SPEED = 4;
+    public static double MAX_LINEAR_ACCELERATION = 0;
+    public static double MAX_ROTATIONAL_SPEED = 0;
+    public static double MAX_ROTATIONAL_ACCELERATION = 0;
+}
